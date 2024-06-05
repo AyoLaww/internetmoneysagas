348f55b0e35c6e95c10ee84a7e53fa3e0854fb01
AyoLaww <104886347+AyoLaww@users.noreply.github.com>
2024-06-05 19:02:15 +0300

    working auth using supabase

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -1,10 +1,39 @@
+"use client"
 
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { Input } from "./ui/input";
+import { FormEvent } from "react";
+import { supabase } from "@/lib/clients/supabase";
+import { Router } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export function LoginCard() {
+  const router = useRouter()
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const email = (document.getElementById('login-email') as HTMLInputElement).value
+    const password = (document.getElementById('login-password') as HTMLInputElement).value
+
+    const { data, error: loginError} = await supabase.auth.signInWithPassword({
+      email, 
+      password
+    })
+
+    if(loginError){
+      console.log('Error logging in:', loginError.message)
+    }
+
+    if(data.user){
+      console.log("User logged in")
+      router.push('/stories')
+    }
+  }
+
+
   return (
     <>
       <Card className="w-96 h-1/2">
@@ -12,10 +41,9 @@ export function LoginCard() {
           <CardTitle>Login</CardTitle>
         </CardHeader>
         <CardContent>
-        <form action="">
-                    <Input type="email" name="email" placeholder="Email" autoComplete="off" className="mt-5" />
-                    <Input type="text" name="username" placeholder="Username" autoComplete="off" className="mt-5" />
-                    <Input type="password" name="password" placeholder="Password" className="mt-5" />
+        <form onSubmit={handleLogin}>
+                    <Input type="email" name="email" id="login-email" placeholder="Email" autoComplete="off" className="mt-5" />
+                    <Input type="password" name="password" placeholder="Password" id="login-password" className="mt-5" />
                     <div className="w-full mt-5 flex flex-row justify-between">
                       <Link href="#" className="hover:underline underline-offset-1 mt-3 text-sm">Forgot Password ?</Link>
                       <Button type="submit" className="self-end">Login</Button>
--- src/components/SignUpDialogComponent.tsx
+++ src/components/SignUpDialogComponent.tsx
@@ -11,8 +11,10 @@ import {
   DialogTrigger,
 } from "./ui/dialog";
 import { Input } from "./ui/input";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
+import { FormEvent, useEffect, useState } from "react";
+import { redirect, usePathname, useRouter } from "next/navigation";
+import { supabase } from "@/lib/clients/supabase";
+
 
 export interface DialogComponentProps{
     title: string
@@ -21,12 +23,50 @@ export interface DialogComponentProps{
 export function DialogComponent({title}: DialogComponentProps) {
     const [open, setOpen] = useState(false)
     const pathname = usePathname()
+    const router = useRouter()
 
     useEffect(()=>{
         if(pathname.startsWith("/Login")){
             setOpen(false)
         }
     }, [pathname])
+
+
+    const handleSignUp = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      
+      const form = e.currentTarget
+      const email = (document.getElementById('email') as HTMLInputElement).value
+      const password = (document.getElementById('password') as HTMLInputElement).value
+      const username = (document.getElementById('username') as HTMLInputElement).value
+
+      const {data, error: signUpError} = await supabase.auth.signUp({
+        email, 
+        password
+      })
+
+      if(signUpError){
+        console.error('Error signing up:', signUpError)
+        return
+      }
+
+      if(data.user) {
+        const { error: profileError } = await supabase
+        .from('profiles')
+        .insert([
+          { id: data.user.id, email, username }
+        ])
+
+        if(profileError){
+          console.error('Error creating Profile:', profileError)
+        } else {
+          console.log("user created")
+          router.push('/stories')
+        }
+      }
+      }
+
+      
 
 
   return (
@@ -39,12 +79,12 @@ export function DialogComponent({title}: DialogComponentProps) {
           <DialogHeader>
             <DialogTitle>Sign Up</DialogTitle>
             <DialogDescription>
-                <form action="">
-                    <Input type="email" name="email" placeholder="Email" autoComplete="off" className="mt-5" />
-                    <Input type="text" name="username" placeholder="Username" autoComplete="off" className="mt-5" />
-                    <Input type="password" name="password" placeholder="Password" className="mt-5" />
+                <form onSubmit={handleSignUp}>
+                    <Input type="email" name="email" id="email" placeholder="Email" autoComplete="off" className="mt-5" />
+                    <Input type="text" name="username" id="username" placeholder="Username" autoComplete="off" className="mt-5" />
+                    <Input type="password" name="password" id="password" placeholder="Password" className="mt-5" />
                     
-                    <div className="flex flex-row justify-end"><Button type="submit" className="mt-5">Sign up</Button></div>
+                    <div className="flex flex-row justify-end"><Button className="mt-5"><input type="submit" value="Sign Up" /></Button></div>
                 </form>
                 <div className="mt-5 flex flex-row justify-center">
                     <Link href="/Login" className="hover:underline underline-offset-1">Already have an account ?</Link>
